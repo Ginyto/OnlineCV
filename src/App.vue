ec7d8f1ba42d9b6ddae9e2a066ea20c66e9a7e1e
Ginyto <agianlucca@gmail.com>
2023-11-15 09:56:51 +0100

    update title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+</script>
+
 <template>
-  <div class=" bg-slate-500">This is a test</div>
+
+  <div class=" h-screen bg-gradient-to-br from-purple-600 via-black to-fuchsia-500">
+    <div class=" h-screen bg-white/5 backdrop-blur">
+      
+    </div>
+  </div>
+
+
 </template>
 
 
